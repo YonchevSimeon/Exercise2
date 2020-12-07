ee2691d0a039d6c5ce7c0a8ca4cb93ad4c189c93
YonchevSimeon <tromavoto@abv.bg>
2020-12-07 14:44:11 +0000

    Initial commit

---
+++ webapp/controller/MainView.controller.js
@@ -0,0 +1,38 @@
+sap.ui.define(["sap/ui/core/mvc/Controller",
+			   "sap/ui/model/json/JSONModel",
+			   "sap/ui/core/Fragment"], 
+	function (Controller, JSONModel, Fragment) {
+	"use strict";
+
+	return Controller.extend("com.syonchev.exercise2.controller.MainView", {
+		onInit: function () {
+				var data = {
+					recipient: {
+						name: 'World'
+					}
+				};
+				var model = new JSONModel(data);
+				this.getView().setModel(model);
+			},
+			onOpenDialog: function () {
+				var oView = this.getView();
+				if (!this.pDialog) {
+					this.pDialog = Fragment.load({
+						id: oView.getId(),
+						name: "com.syonchev.exercise2.view.HelloDialog",
+						controller: this
+					}).then(function (oDialog) {
+						oView.addDependent(oDialog);
+						return oDialog;
+					});
+				}
+				this.pDialog.then(function (oDialog) {
+					oDialog.open();
+				});
+			},
+			onCloseDialog : function () {
+			this.byId("helloDialog").close();
+		}
+		});
+	}
+);
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/syonchev/exercise2/test/unit/controller/MainView.controller"
+], function () {
+	"use strict";
+});
